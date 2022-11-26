2df3b63e8ebd234bf8e9172caeda2685e0a66c57
@erick <erick.hx22@gmail.com>
2022-11-26 13:22:46 -0500

    chore: add styles to all page (#1)

--- src/pages/AllPage/AllPage.tsx
+++ src/pages/AllPage/AllPage.tsx
@@ -1,5 +1,30 @@
+import { ServiceCard } from "./components/ServiceCard";
+
+import { ContainerStyled } from "./styles";
+
+const servicesList = [
+  { id: "1", title: "Electricidad", description: "description" },
+  { id: "2", title: "Auxilio Mecanico", description: "description" },
+  { id: "3", title: "Chofer reemplazo", description: "description" },
+  { id: "4", title: "Medico a domicilio", description: "description" },
+  { id: "5", title: "Ambulancia", description: "description" },
+];
+
 const AllPage = () => {
-  return <div>AllPage</div>;
+  return (
+    <ContainerStyled>
+      <section className="list">
+        {servicesList.map(({ id, title, description }) => (
+          <ServiceCard
+            key={id}
+            title={title}
+            description={description}
+            id={id}
+          />
+        ))}
+      </section>
+    </ContainerStyled>
+  );
 };
 
 export default AllPage;
---
+++ src/pages/AllPage/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const ContainerStyled = styled.div`
+  .list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 20px;
+  }
+`;
