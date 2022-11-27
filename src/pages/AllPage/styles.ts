f850bcd8f69c98411773b4a43098634f378a7f3a
@erick <erick.hx22@gmail.com>
2022-11-26 19:58:07 -0500

    fix: repair styles in container page (#2)

--- src/pages/AllPage/styles.ts
+++ src/pages/AllPage/styles.ts
@@ -1,8 +1,22 @@
 import styled from "styled-components";
+
 export const ContainerStyled = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr auto;
+  }
   .list {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     grid-gap: 20px;
+    order: 2;
+    @media screen and (min-width: 768px) {
+      order: 1;
+    }
+  }
+  .form {
+    min-width: 300px;
+    order: 1;
   }
 `;
