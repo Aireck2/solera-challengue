63bd1b2c7b7bba4e614ecbccb55ca48a0469b216
@erick <erick.hx22@gmail.com>
2022-11-26 19:53:18 -0500

    fix: repair height styles in card (#2)

--- src/components/basics/Card/styles.ts
+++ src/components/basics/Card/styles.ts
@@ -3,19 +3,20 @@ import styled from "styled-components";
 export const CardStyled = styled.div`
   border-radius: 4px;
   border: 1px solid #d9d9d9;
-  font-family: sans-serif;
+  display: flex;
+  flex-direction: column;
   .title {
     font-weight: bold;
     font-size: 18px;
     margin-bottom: 10px;
   }
-  .description {
-  }
   .content {
     padding: 1rem;
     font-size: 14px;
+    height: 100%;
   }
   .actions {
+    justify-self: flex-end;
     border-top: 1px solid #d9d9d9;
     padding: 1rem;
     display: flex;
