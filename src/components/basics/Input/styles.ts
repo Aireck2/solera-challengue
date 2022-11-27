41fc77d59766452f92d956d54335496e498a54a8
@erick <erick.hx22@gmail.com>
2022-11-26 19:56:31 -0500

    feat: add input component (#2)

---
+++ src/components/basics/Input/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const InputStyled = styled.input`
+  border-radius: 4px;
+  outline: none;
+  border: 1px solid #d9d9d9;
+  padding: 0.3rem;
+  width: 100%;
+`;
