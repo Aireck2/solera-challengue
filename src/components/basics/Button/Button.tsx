10db57b971bbf3696d4f0099a75041eef53cd165
@erick <erick.hx22@gmail.com>
2022-11-26 19:52:28 -0500

    feat: add html type prop in button (#2)

--- src/components/basics/Button/Button.tsx
+++ src/components/basics/Button/Button.tsx
@@ -5,8 +5,13 @@ import { ButtonStyled, ButtonTypes } from "./styles";
 interface ButtonProps {
   children: JSX.Element | string;
   type?: ButtonTypes;
+  htmlType?: "button" | "submit" | "reset";
 }
 
-export const Button: FC<ButtonProps> = ({ children, type }) => {
-  return <ButtonStyled $type={type || "default"}>{children}</ButtonStyled>;
+export const Button: FC<ButtonProps> = ({ children, type, htmlType }) => {
+  return (
+    <ButtonStyled $type={type || "default"} type={htmlType}>
+      {children}
+    </ButtonStyled>
+  );
 };
