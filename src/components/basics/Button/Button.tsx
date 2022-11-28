99940591def26a02ffcc82814cd42ab361ddae6e
@erick <erick.hx22@gmail.com>
2022-11-27 21:24:02 -0500

    fix: add proper button prop types (#3)

--- src/components/basics/Button/Button.tsx
+++ src/components/basics/Button/Button.tsx
@@ -2,15 +2,24 @@ import { FC } from "react";
 
 import { ButtonStyled, ButtonTypes } from "./styles";
 
-interface ButtonProps {
+type HtmlButtonProps = Omit<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  "type"
+>;
+interface ButtonProps extends HtmlButtonProps {
   children: JSX.Element | string;
   type?: ButtonTypes;
   htmlType?: "button" | "submit" | "reset";
 }
 
-export const Button: FC<ButtonProps> = ({ children, type, htmlType }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  type,
+  htmlType,
+  ...rest
+}) => {
   return (
-    <ButtonStyled $type={type || "default"} type={htmlType}>
+    <ButtonStyled $type={type || "default"} type={htmlType} {...rest}>
       {children}
     </ButtonStyled>
   );
