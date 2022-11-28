830de0b790fd89645f47b5915ba3d758743ecd90
@erick <erick.hx22@gmail.com>
2022-11-27 21:29:09 -0500

    refactor: add listservices from useservices hook (#3)

--- src/pages/AllPage/AllPage.tsx
+++ src/pages/AllPage/AllPage.tsx
@@ -1,21 +1,17 @@
 import { ServiceCard } from "./components/ServiceCard";
 import { CreateService } from "./components/Form/CreateService";
-
-import { servicesList } from "../../../dataMock";
+import { useServices } from "@/hooks";
 
 import { ContainerStyled } from "./styles";
 
 const AllPage = () => {
+  const { listServices } = useServices();
+
   return (
     <ContainerStyled>
       <section className="list">
-        {servicesList.map(({ id, title, description }) => (
-          <ServiceCard
-            key={id}
-            name={title}
-            description={description}
-            id={id}
-          />
+        {listServices.map(({ id, name, description }) => (
+          <ServiceCard key={id} name={name} description={description} id={id} />
         ))}
       </section>
       <section className="form">
