beedd969cc613af89e173ffa89ffcee6f5dd104f
@erick <erick.hx22@gmail.com>
2022-11-27 21:28:28 -0500

    chore: add save method in create service form (#3)

--- src/pages/AllPage/components/Form/CreateService.tsx
+++ src/pages/AllPage/components/Form/CreateService.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 
-import { Card } from "@/components/basics";
+import { ServiceValues } from "@/models/service";
+import { useServices } from "@/hooks";
 
-import { ServiceForm, ServiceValues } from "./ServiceForm";
+import { ServiceForm } from "./ServiceForm";
 
 export const CreateService: FC = () => {
+  const { saveOne } = useServices();
+
   const initValues: ServiceValues = { name: "", description: "", label: "" };
 
   const handleSubmit = (values: ServiceValues) => {
-    setTimeout(() => {
-      console.table(values);
-    }, 400);
+    const id = new Date().toISOString();
+    saveOne({ id, ...values });
   };
   return <ServiceForm handleSubmit={handleSubmit} initialValues={initValues} />;
 };
