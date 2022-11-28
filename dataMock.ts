335fac929819a0af979b35fd7743092cc5868ae7
@erick <erick.hx22@gmail.com>
2022-11-27 21:23:49 -0500

    feat: remove datamock (#3)

--- dataMock.ts
+++
@@ -1,7 +0,0 @@
-export const servicesList = [
-  { id: "1", title: "Electricidad", description: "description" },
-  { id: "2", title: "Auxilio Mecanico", description: "description" },
-  { id: "3", title: "Chofer reemplazo", description: "description" },
-  { id: "4", title: "Medico a domicilio", description: "description" },
-  { id: "5", title: "Ambulancia", description: "description" },
-];
